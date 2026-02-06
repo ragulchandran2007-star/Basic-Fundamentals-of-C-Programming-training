449642776cdc33df59bb9865cde2c36313074d6c
ragulchandran2007-star <ragulchandran2007@gmail.com>
2026-02-06 13:44:16 +0530

    Create Water Bottles 5.c

---
+++ Water Bottles 5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int numWaterBottles(int numBottles, int numExchange) { 
+
+        int total=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            int newBottle=empty/numExchange;
+            total=total+newBottle;
+            empty=newBottle+(empty%numExchange);
+        }
+        return total;
+    }
