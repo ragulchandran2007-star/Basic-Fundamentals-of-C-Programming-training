5fb93abe26dc7bfc8cc9c4cab8218680665fa63f
ragulchandran2007-star <ragulchandran2007@gmail.com>
2026-02-06 13:32:14 +0530

    Create Is it an Armstrong Number? .c

---
+++ Is it an Armstrong Number? .c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+
+    int num, count=0,original, rem;
+    int temp,sum = 0;
+    scanf("%d",&num);
+    original=num;
+    while(num!=0){
+        num/=10;
+        count++;
+    }
+    temp = original;
+    while(temp!=0){
+        rem = temp%10;
+        sum= sum+pow(rem,count);
+        temp/=10;
+    }
+    
+    if(sum==original){
+        printf("true");
+    }
+    else{
+        printf("false");
+    }
+    
+    
+    return 0;
+}
